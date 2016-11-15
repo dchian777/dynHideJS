fec58d4f92d368b26adf11636317f62c978810d8
David Chian <dchian777@gmail.com>
2016-11-15 17:39:23 +0800

    Add files via upload
    First Iteration

---
+++ dynHide.js
@@ -0,0 +1,69 @@
+//Dynamic Hide Module
+
+//Primer - Primes Text body for dynamic Hide
+    //id is the id of the element
+    //decoder is the id of the dynamic source
+function DHprimer(id, decoder){
+
+  //Converts text into array of single characters
+  var textArray = document.getElementById(id).innerHTML.split('');
+
+  //Clears element
+  document.getElementById(id).innerHTML = "";
+
+  //Creates a SPAN for each character in the element with a class of *-DH
+  for (i in textArray){
+    var subDiv = document.createElement("span");
+    var divText = document.createTextNode(textArray[i]);
+    subDiv.appendChild(divText);
+    subDiv.className = textArray[i]+"-DH";
+    document.getElementById(id).appendChild(subDiv);
+  }
+
+  //Creates a hidden div that stores the characters hidden. This is to allow for undoing hidden characters
+  var hiddenDiv = document.createElement("div");
+  hiddenDiv.id = "dynHide-hidden-div";
+  hiddenDiv.style = "display: none";
+  document.getElementById(id).appendChild(hiddenDiv);
+
+  //Focuses the user onto decoder div
+  document.getElementById(decoder).focus();
+
+}
+
+
+//Dynamic Hiding of characters.
+function dynHide(decoder){
+  //Shows previously hidden characters
+
+    //splits hidden div innerHTML into an array of single characters
+var hiddenPrev = document.getElementById('dynHide-hidden-div').innerHTML.split('');
+
+    //for each character in hidden div, find the corresponding className
+for (i in hiddenPrev){
+  search = document.getElementsByClassName(hiddenPrev[i] + "-DH");
+
+    //for each element with classname found, reset the style
+  for (x in search){
+    search[x].style = "opacity: 1";
+  }
+}
+
+//Hides current character choice
+
+  //Replace hidden div innerHTML with current choices
+  document.getElementById('dynHide-hidden-div').innerHTML = document.getElementById(decoder).value;
+
+  //splits hidden div innerHTML into an array of single characters
+var hiddenCur = document.getElementById('dynHide-hidden-div').innerHTML.split('');
+
+  //for each character in hidden div, find the corresponding className
+  for (i in hiddenCur){
+    search = document.getElementsByClassName(hiddenCur[i]+"-DH");
+
+  //for each element in className found, change the style
+    for (x in search){
+      search[x].style = "opacity: 0.3";
+    }
+  }
+}
